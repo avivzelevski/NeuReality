c15862f8813c98e7159d4e5d9d29efa15756518a
avivzelevski <aviv.zelevski@gmail.com>
2022-09-17 19:01:58 +0300

    adding files

---
+++ ec2.tf
@@ -0,0 +1,108 @@
+provider "aws" {
+  region                  = "eu-central-1"
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "eu-central-1a"
+  tags = {
+    Name = "Default subnet"
+  }
+}
+
+resource "aws_security_group" "secure" {
+  name        = "secure"
+  description = "Allow HTTP, SSH inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description = "k8s"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description = "k8s-1"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "k8s-2"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    description = "ping"
+    from_port   = 0
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+  tags = {
+    Name = "security-wizard"
+  }
+}
+
+resource "aws_instance" "master" {
+  ami           = "ami-0f64f746a3cb9a16e"
+  instance_type = "t3.micro"
+  key_name = "aviv"
+ # user_data = "fd50d738924f03fa08bbabe03058dcdcd20d0986"
+  security_groups = [ "${aws_security_group.secure.name}" ]
+  tags = {
+    Name = "Master"
+  }
+  root_block_device {
+    delete_on_termination = "true"
+  }
+}
+
+resource "aws_instance" "slave" {
+  ami = "ami-0f64f746a3cb9a16e"
+  instance_type = "t3.micro"
+  key_name = "aviv"
+  #count = 2
+ # user_data = "fd50d738924f03fa08bbabe03058dcdcd20d0986"
+  security_groups = [ "${aws_security_group.secure.name}" ]
+  tags = {
+    Name = "Slave"
+  }
+  root_block_device {
+    delete_on_termination = "true"
+  }
+}
